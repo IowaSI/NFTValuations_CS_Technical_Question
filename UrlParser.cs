5670811de1b41edfeaa7f07a2000c406195ae566
unknown <ilya.svistunov@netsoftware.pl>
2023-05-14 15:49:26 +0200

    added enum, added new url type

--- UrlParser.cs
+++ UrlParser.cs
@@ -1,25 +1,30 @@
-﻿using Org.BouncyCastle.Utilities.IO;
+﻿using NFTValuations.Enums;
+using Org.BouncyCastle.Utilities.IO;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace NFTValuations
 {
-    internal static class UriParser
+    internal static class UrlParser
     {
         internal static string Parse(string url)
         {
             switch (UriType(url))
             {
-                case "base64":
+                case UrlEnums.UrlType.base64:
                     var replacedUri = url.Replace("data:application/json;base64,", "");
                     byte[] data = Convert.FromBase64String(replacedUri);
                     return Encoding.UTF8.GetString(data);
-                    
-                case "ipfs":
+
+                case UrlEnums.UrlType.json:
+                    return url.Replace("data:application/json;utf8,", "");
+
+                case UrlEnums.UrlType.ipfs:
                     var newUrl = $"https://ipfs.io/ipfs/{url.Replace("ipfs://","")}";
                     return GetContent(newUrl);
                     
@@ -28,24 +33,29 @@ namespace NFTValuations
             }            
         }
 
-        private static string UriType(string uri)
+        private static UrlEnums.UrlType UriType(string uri)
         {
             if (uri.StartsWith("data:application/json;base64,", StringComparison.OrdinalIgnoreCase))
-                return "base64";
+                return UrlEnums.UrlType.base64;
 
             if (uri.StartsWith("ipfs://", StringComparison.OrdinalIgnoreCase))
-                return "ipfs";
+                return UrlEnums.UrlType.ipfs;
 
-            return uri;
+            if (uri.StartsWith("data:application/json;", StringComparison.OrdinalIgnoreCase))
+                return UrlEnums.UrlType.json;
+
+            return UrlEnums.UrlType.origin;
         }
 
         private static string GetContent(string url)
         {
             try
             {
-                using (HttpClient client = new HttpClient())
+                using (var client = new HttpClient())
                 {
-                    HttpResponseMessage response = client.GetAsync(url).Result;
+                    client.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", "Mozilla/5.0 (Windows NT 6.2; WOW64; rv:19.0) Gecko/20100101 Firefox/19.0");
+                    
+                    var response = client.GetAsync(url).Result;
                     if (response.IsSuccessStatusCode)
                     {
                         var responseBody = response.Content.ReadAsStringAsync().Result;
